35ba85ed75dfe69b6976854df1e931c68de3f240
Mahima Srivastava <118749197+MahimaSrivastava20@users.noreply.github.com>
2026-02-03 14:44:59 +0530

    question3

---
+++ TopBrains-Practice/Inventory/question3/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+using System.Globalization;
+using System.Text;
+
+class Program
+{
+    static string FormatProduct(string input)
+    {
+        input = input.Trim();
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (i == 0 || input[i] != input[i - 1])
+            {
+                result.Append(input[i]);
+            }
+        }
+
+        string cleaned = string.Join(" ",
+            result.ToString().Split(' ', StringSplitOptions.RemoveEmptyEntries));
+
+        return CultureInfo.CurrentCulture.TextInfo
+            .ToTitleCase(cleaned.ToLower());
+    }
+
+    static void Main()
+    {
+        string input = " llapppptop bag ";
+        Console.WriteLine(FormatProduct(input));
+    }
+}
