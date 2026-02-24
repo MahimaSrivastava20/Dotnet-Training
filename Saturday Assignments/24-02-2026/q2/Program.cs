002a6315e0e9181c58f4471b4cbe5906138b7710
Mahima Srivastava <118749197+MahimaSrivastava20@users.noreply.github.com>
2026-02-24 14:37:44 +0530

    LINQ DUPLICATE ITEM QUESTION

---
+++ Saturday Assignments/24-02-2026/q2/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> products = new List<string>
+        {
+            "Pen","Book","Pen","Pencil","Book"
+        };
+
+        // LINQ to find duplicates
+        List<string> duplicates = products
+                                  .GroupBy(p => p)
+                                  .Where(g => g.Count() > 1)
+                                  .Select(g => g.Key)
+                                  .ToList();
+
+     
+        Console.WriteLine("Duplicate Products:");
+        foreach (var item in duplicates)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
+
